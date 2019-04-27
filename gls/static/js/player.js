6cd548cef790c9497de5f6f97bb59953ed47bbf8
astprasa <aprasad34@sapient.com>
2019-04-27 13:16:44 +0530

    reading the json data

--- gls/static/js/player.js
+++ gls/static/js/player.js
@@ -1,2 +1,41 @@
 //ADD YOUR CODE HERE.
-console.log("test");
+
+class Guide {
+    constructor(element) {
+        this.$element = $(element);
+        this._init();
+    }
+
+    _init() {
+        this._handleRequest();
+    }
+
+    _createRequest () {
+        const ajaxOptions = {
+        method:'GET',
+        url: this.$element.data('url'),
+        data: '',
+        dataType: 'json'
+        };
+        return $.ajax(ajaxOptions);
+    }
+
+    _handleRequest () {
+        this._createRequest()
+        .then((result) => {
+           console.log(result);
+        },
+        (error) => {
+            console.log(error);
+        });
+    }
+
+}
+
+function attach(selector = ".guide") {
+    document.querySelectorAll(selector).forEach(el => {
+        return new Guide(el);
+    });
+}
+
+attach();
